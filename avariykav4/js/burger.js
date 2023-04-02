f520e7abeed18a3f0353b88829487376003e960a
Alex T <78613215+southernmoon@users.noreply.github.com>
2023-04-02 19:23:25 +0700

    Add files via upload

---
+++ avariykav4/js/burger.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector('.menu_icon');
+if(iconMenu){
+	const menuBody = document.querySelector('.menu_body');
+	iconMenu.addEventListener('click', function(e){
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
