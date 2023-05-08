270ba47ebd82a56ba4f6caeed65622b6fe8498ff
Alex T <78613215+southernmoon@users.noreply.github.com>
2023-05-08 17:24:10 +0700

    Delete avariyka5 directory

--- avariyka5/js/burger.js
+++
@@ -1,9 +0,0 @@
-const iconMenu = document.querySelector('.menu_icon');
-if(iconMenu){
-	const menuBody = document.querySelector('.menu_body');
-	iconMenu.addEventListener('click', function(e){
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-	});
-}
